6e9fdda317557f949f9e3343ac3a956a18789464
rezha4 <rezha.bahari@gmail.com>
2023-11-06 13:35:51 +0700

    deployed

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require("express");
 
 const app = express();
 
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const uri = process.env.SECRET_KEY;
+console.log(`uri is ${uri}`)
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -11,4 +26,9 @@ app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 
-app.listen(3000);
+const hostname = "0.0.0.0";
+const port = process.env.port || 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
